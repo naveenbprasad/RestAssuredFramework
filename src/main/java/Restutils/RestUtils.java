7b8fc38fa8bd1ae4e4319f2c87e61af8bba017f5
NaveenPrasad <naveenbprasad@gmail.com>
2024-07-29 18:34:28 +0530

    Naveen - Refactored extent report to pretty format and added querablerequestspec in util

--- src/main/java/Restutils/RestUtils.java
+++ src/main/java/Restutils/RestUtils.java
@@ -3,35 +3,57 @@ package Restutils;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.QueryableRequestSpecification;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.SpecificationQuerier;
+import reporting.ExtentReportManager;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RestUtils {
 
-//    public RequestSpecification getRequestSpecification(String e)
+    public static RequestSpecification getRequestSpecification(String endpoint, Object payload, Map<String,String> headers){
+        return RestAssured.given().baseUri(endpoint)
+                .contentType(ContentType.JSON)
+                .headers(headers)
+                .body(payload);
+    }
+
+    private static void printRequestLogInReport(RequestSpecification requestSpecification) {
+        QueryableRequestSpecification queryableRequestSpecification= SpecificationQuerier.query(requestSpecification);
+        ExtentReportManager.logInfoDetails("Endpoint is " + queryableRequestSpecification.getBaseUri());
+        ExtentReportManager.logInfoDetails("Method is " + queryableRequestSpecification.getMethod());
+        ExtentReportManager.logInfoDetails("Headers are ");
+        ExtentReportManager.logHeaders(queryableRequestSpecification.getHeaders().asList());
+        ExtentReportManager.logInfoDetails("Request body is ");
+        ExtentReportManager.logJson(queryableRequestSpecification.getBody());
+    }
+
+    private static void printResponseLogInReport(Response response) {
+        ExtentReportManager.logInfoDetails("Response status is " + response.getStatusCode());
+        ExtentReportManager.logInfoDetails("Response Headers are ");
+        ExtentReportManager.logHeaders(response.getHeaders().asList());
+        ExtentReportManager.logInfoDetails("Response body is ");
+        ExtentReportManager.logJson(response.getBody().prettyPrint());
+    }
 
 
     public static Response performPost(String endpoint, String payload, Map<String,String> headers){
 
-        return RestAssured.given().baseUri(endpoint).log().all()
-                .contentType(ContentType.JSON)
-                .headers(headers)
-                .body(payload)
-                .when().post().then().log().all()
-                .extract().response();
+        RequestSpecification requestSpecification=  getRequestSpecification(endpoint,payload,headers);
+        Response response = requestSpecification.post();
+        printRequestLogInReport(requestSpecification);
+        printResponseLogInReport(response);
+        return response;
 
     }
 
     public static Response performPost(String endpoint, Map<String,Object> payload, Map<String,String> headers){
-
-        return RestAssured.given().baseUri(endpoint).log().all()
-                .contentType(ContentType.JSON)
-                .headers(headers)
-                .body(payload)
-                .when().post().then().log().all()
-                .extract().response();
-
+        RequestSpecification requestSpecification=  getRequestSpecification(endpoint,payload,headers);
+        Response response = requestSpecification.post();
+        printRequestLogInReport(requestSpecification);
+        printResponseLogInReport(response);
+        return response;
     }
 }
--- src/main/java/reporting/Setup.java
+++ src/main/java/reporting/Setup.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
 
 import java.util.List;
 
@@ -22,6 +23,13 @@ public class Setup implements ITestListener {
     public void onFinish(ITestContext context) {
         if (extentReports != null)
             extentReports.flush();
+    }
+
+
+    public void onTestStart(ITestResult result) {
+        ExtentTest test = extentReports.createTest("Test Name " + result.getTestClass().getName() + " - " + result.getMethod().getMethodName(),
+                result.getMethod().getDescription());
+        extentTest.set(test);
     }
 
 
